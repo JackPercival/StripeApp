9d97ff193f759b27b53b0707d3d7d6fdd1a35dfa
Jack Percival <jackpercival7@gmail.com>
2024-07-26 22:47:45 -0700

    Add env

--- express-backend/routes/checkout.js
+++ express-backend/routes/checkout.js
@@ -1,19 +1,20 @@
 const stripe = require('stripe')('sk_test_51Pgx1GRsYhiCBNPNE4I1gmBezaXKNduWA6jZFBixDKqa9934fR9IsgfaOcM1JWjdS1yqwJDV4Ie0PvFqDlL03Sbg00R6O3EhmP');
 const express = require('express');
 const router = express.Router();
+const URL = process.env.URL
 
 router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
+        price: 'prod_QY50bOgDirRsP6',
         quantity: 1,
       },
     ],
     mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    success_url: `${URL}?success=true`,
+    cancel_url: `${URL}?canceled=true`,
   });
 
   res.redirect(303, session.url);
