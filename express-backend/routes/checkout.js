4ac57ed66a9c05cc08d133e2363b42358af843fe
Jack Percival <jackpercival7@gmail.com>
2024-07-27 12:24:38 -0700

    Update

--- express-backend/routes/checkout.js
+++ express-backend/routes/checkout.js
@@ -1,14 +1,16 @@
-const stripe = require('stripe')('sk_test_51Pgx1GRsYhiCBNPNE4I1gmBezaXKNduWA6jZFBixDKqa9934fR9IsgfaOcM1JWjdS1yqwJDV4Ie0PvFqDlL03Sbg00R6O3EhmP');
+const SECRET_KEY = process.env.SECRET_KEY
+const stripe = require('stripe')(SECRET_KEY);
+const URL = process.env.URL
+
 const express = require('express');
 const router = express.Router();
-const URL = process.env.URL
 
 router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'prod_QY50bOgDirRsP6',
+        price: 'price_1PgyqPRsYhiCBNPNDDry563V',
         quantity: 1,
       },
     ],
